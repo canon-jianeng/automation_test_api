27386149e9b59a4cd57ed784f6811c02db2442f2
unknown <Canon.Chen@yatsenglobal.com>
2020-03-09 21:17:09 +0800

    first commit

---
+++ api_test/Util/Signature.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/13 22:42
+# @Author  : canon
+# @Project : yatsen_automation_api
+# @FileName: Signature.py
+# @Software: PyCharm
+
+import hashlib
+import hmac
+from base64 import b64encode
+
+from api_test.config.DingConfig import APPSECRET
+
+
+def signature(code):
+    """
+    hmacsha256算法
+    :param code:
+    :return:
+    """
+    appkey = APPSECRET  # miyao
+
+    # hmac_sha256加密
+    signature = hmac.new(bytes(appkey, encoding='utf-8'), bytes(code, encoding='utf-8'),
+                         digestmod=hashlib.sha256).digest()
+
+    # 二进制转为HEX
+    HEX = str(b64encode(signature), encoding='utf-8')
+    return HEX
+
+
+if __name__ == '__main__':
+    print(signature('1234567890123'))
---
+++ api_test/config/DingConfig.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/13 22:12
+# @Author  : canon
+# @Project : yatsen_automation_api
+# @FileName: DingConfig.py
+# @Software: PyCharm
+
+APPID = 'dingoapfjxo0dzezwe47sy'
+APPSECRET = '5_6b7h8eogyS1kstGWigpmuQbQUd585Vlc9ftjlcz48_avtLGQ5glolATWl3HFOf'
